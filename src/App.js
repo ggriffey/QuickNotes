745c5407da5b6e0224d74a49e6da19b03f1a7b06
Greyson Griffey <ggriffey@greysons-air.myfiosgateway.com>
2022-10-03 13:19:06 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import NoteList from "./components/NoteList";
 
-function App() {
+const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note",
+      date: "10/10/10",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note",
+      date: "12/23/99",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note",
+      date: "6/10/10",
+    },
+    {
+      id: nanoid(),
+      text: "This is my fourth note",
+      date: "10/17/10",
+    },
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <NoteList notes={notes} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/AddNote.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const AddNote = () => {
+  const [noteText, setNoteText] = useState("");
+
+  const handleChange = (event) => {
+    setNoteText(event.target.value);
+  };
+
+  const handleSaveClick = () => {};
+
+  return (
+    <div className="note new">
+      <textarea
+        rows="8"
+        cols="10"
+        placeholder="Type to add a new note"
+        value={noteText}
+        onChange={handleChange}
+      ></textarea>
+      <div className="note-footer">
+        <small> 300 chars remaining</small>
+        <button className="save" onClick={handleSaveClick}>
+          {" "}
+          Save{" "}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AddNote;
